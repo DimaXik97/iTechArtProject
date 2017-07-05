96029ea5a2937424372ae0655d151a7ac60cf458
dimaxik97 <dimaxik97@gmail.com>
2017-07-05 17:37:22 +0300

    add structure and menu in AboutPage

---
+++ UI/js/googleMap.js
@@ -0,0 +1,11 @@
+function initMap() {
+    var uluru = {lat: 53.88841, lng: 27.544509};
+    var map = new google.maps.Map(document.getElementById('content'), {
+        zoom: 16,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
