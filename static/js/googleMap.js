79bd0e46c06a1ac6e7d17a5b87c071b0187333ff
dimaxik97 <dimaxik97@gmail.com>
2017-07-12 16:19:51 +0300

    finished about page

--- static/js/googleMap.js
+++ static/js/googleMap.js
@@ -1,6 +1,6 @@
 function initMap() {
     var coordinates = {lat: 53.88841, lng: 27.544509};
-    var map = new google.maps.Map(document.getElementById('contacts'), {
+    var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 17,
         center: coordinates
     });
--- static/js/menu.js
+++ static/js/menu.js
@@ -1,12 +1,10 @@
 function openlink(element){
-    linkMenu=element.getAttribute("class");
     divsContent=document.getElementsByClassName("content");
     console.log(divsContent);
     attribureClass="content";
-    console.log(linkMenu);
     for(i=0;i<divsContent.length;i++)
     {
         divsContent[i].setAttribute("class",attribureClass);
     }
-    document.getElementById(linkMenu).setAttribute("class",attribureClass+" active");
+    document.getElementById(element).setAttribute("class",attribureClass+" active");
 }
