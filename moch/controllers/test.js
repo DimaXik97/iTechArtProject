7a8ed335612300bb4f8980f06de9d0d24fc5be69
dimaxik97 <dimaxik97@gmail.com>
2017-07-28 17:21:18 +0300

    added moch questions and moch tests

--- moch/controllers/test.js
+++ moch/controllers/test.js
@@ -2,13 +2,38 @@ const express = require('express');
 const test = require('./../test');
 module.exports = () => {
     const router = express.Router();
-    router.get('/',(req, res) =>
+    router.get('/:id/:test',(req, res) =>
+    {
+        questions=[];
+        test.forEach(function(element) {
+           if(element.id==req.params.id){
+                element.tests.forEach(tElement=>{
+                    if(tElement.id==req.params.test)
+                        questions=tElement.questions;
+                })
+            }
+        });
+        res.send(questions);
+    });
+    router.get('/:id',(req, res) =>
     {
         tests=[];
         test.forEach(function(element) {
-            tests.push({id: element.id, name: element.name});
+            if(element.id==req.params.id){
+                element.tests.forEach(tElement=>{
+                    tests.push({id: tElement.id, name: tElement.name});
+                })
+            }
         });
         res.send(tests);
+    });
+    router.get('/',(req, res) =>
+    {
+        categories=[];
+        test.forEach(function(element) {
+            categories.push({id: element.id, name: element.name});
+        });
+        res.send(categories);
     });
     return router;
 }
