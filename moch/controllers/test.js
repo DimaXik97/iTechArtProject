b13b983359b140022ea800366552b125615e2e2e
dimaxik97 <dimaxik97@gmail.com>
2017-07-28 17:08:44 +0300

    added moch test

--- moch/controllers/test.js
+++ moch/controllers/test.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const test = require('./../test');
 module.exports = () => {
     const router = express.Router();
     router.get('/',(req, res) =>
     {
-        res.send("OK");
+        tests=[];
+        test.forEach(function(element) {
+            tests.push({id: element.id, name: element.name});
+        });
+        res.send(tests);
     });
     return router;
 }
