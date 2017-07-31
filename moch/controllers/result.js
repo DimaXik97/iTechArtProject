b5bf563a8d27d3fa18e1d64e442cc555c3e49adf
dimaxik97 <dimaxik97@gmail.com>
2017-07-31 14:04:00 +0300

    added moch result

--- moch/controllers/result.js
+++ moch/controllers/result.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const results = require('./../results');
+
 module.exports = () => {
     const router = express.Router();
     router.get('/',(req, res) =>
     {
-        res.send("OK");
+        tests=[];
+        results.forEach(function(element) {
+            if(req.query.user)
+            {
+                if(req.query.user!=element.idUser) return;
+            }
+            if(req.query.test)
+            {
+                if(req.query.test!=element.idTest) return;
+            }
+            tests.push({id:element.id, user:element.idUser, test: element.idTest, date: element.date});
+        });
+        res.send(tests);
     });
+    router.get('/:id',(req, res) =>
+    {
+        results.forEach(function(element) {
+            res.send(element);
+        });
+    });
+
     return router;
 }
